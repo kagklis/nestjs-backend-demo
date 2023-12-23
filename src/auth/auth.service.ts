df8c01c6303b4ba6e7a312757e294e729393623f
kagklis <kagklis@gmail.com>
2023-12-23 15:58:37 +0200

    Hash password before storing it.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDTO } from './dtos/register.dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -9,10 +10,11 @@ export class AuthService {
 
   async register(dto: RegisterDTO) {
     try {
+      const hash = await argon.hash(dto.password);
       const user = await this.prisma.user.create({
         data: {
           email: dto.email,
-          hash: dto.password,
+          hash,
           firstName: dto.firstName,
           lastName: dto.lastName,
         },
@@ -22,7 +24,7 @@ export class AuthService {
           createdAt: true,
         },
       });
-      console.log(user);
+      return user;
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
